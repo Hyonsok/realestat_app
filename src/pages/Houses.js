106d669f631d8c690c0a1ea5d11e3c928d434504
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2022-05-03 00:04:02 -0700

    created pages
    created home, houses, signup pages

---
+++ src/pages/Houses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import { Button } from '../components/Button';
+
+function Houses() {
+  return (
+      <div>
+          This is Houses page
+      </div>
+  );
+}
+
+export default Houses;
